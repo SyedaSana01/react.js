405e9c9ebe61e4bf4aabf6f2681e5da6c31959e6
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2024-05-03 01:40:27 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>this is a paaragraph</p>
+  </div>,
+  document.getElementById("root")
+);
